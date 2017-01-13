0df79ebb2d2321bffaff7dc04ec70ba7f0640935
iOSMoonSand <alexisschreier@gmail.com>
2017-01-13 00:18:21 -0800

    ran into Instagram API permissions, added JSON serialization for Insta stats

--- my insta/my insta/ImagePostCell.m
+++ my insta/my insta/ImagePostCell.m
@@ -15,22 +15,39 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *postImageView;
 @property (weak, nonatomic) IBOutlet UILabel *postCaptionLabel;
-@property (weak, nonatomic) IBOutlet UILabel *postCommentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *commentsCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateCreatedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *filterLabel;
+@property (weak, nonatomic) IBOutlet UILabel *likesCountLabel;
+@property (weak, nonatomic) IBOutlet UITextView *linkTextView;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tagsLabel;
 
 @end
 
 @implementation ImagePostCell
 
 - (void) performWith: (ImagePost *)item {
-    NSString *caption = [NSString stringWithFormat: @"%@ %@", self.post.user.username, self.post.caption];
-    self.postCaptionLabel.text = caption;
-    NSMutableString *commentString = [[NSMutableString alloc] init];
-    for (Comment *comment in self.post.comments) {
-        NSString *oneComment = [NSString stringWithFormat: @"%@ %@\n", comment.author.username, comment.text];
-        [commentString appendString: oneComment];
-    }
-    self.postCommentLabel.text = commentString;
     self.postImageView.image = self.post.image;
+    NSString *caption = [NSString stringWithFormat: @"%@", self.post.caption];
+    self.postCaptionLabel.text = caption;
+    self.commentsCountLabel.text = item.commentsCount;
+    self.dateCreatedLabel.text = item.dateCreated;
+    self.filterLabel.text = item.filter;
+    self.likesCountLabel.text = item.likesCount;
+    NSMutableAttributedString * linkString = [[NSMutableAttributedString alloc] initWithString:@"Tap here to see your original post!"];
+    [linkString addAttribute: NSLinkAttributeName value: item.link range: NSMakeRange(0, linkString.length)];
+    self.linkTextView.attributedText = linkString;
+    self.locationLabel.text = item.location;
+    self.tagsLabel.text = item.tags;
+    
+    
+//    NSMutableString *commentString = [[NSMutableString alloc] init];
+//    for (Comment *comment in self.post.comments) {
+//        NSString *oneComment = [NSString stringWithFormat: @"%@ %@\n", comment.author.username, comment.text];
+//        [commentString appendString: oneComment];
+//    }
+//    self.postCommentLabel.text = commentString;
 }
 
 @end
